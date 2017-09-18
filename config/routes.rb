24e5a413902c7949951d5a814bd8164abb9aae5e
EAPCMC <E.A.PEENE@GMAIL.com>
2017-09-18 17:54:09 +0200

    Add show page after sign in for Admin and Student

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions" }
 
   resources :students, only: [:show]
-
+  resources :admins, only: [:show]
 end
