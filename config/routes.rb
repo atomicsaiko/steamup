88c40fff048163dba65a370623f4d5db9d6b5607
EAPCMC <E.A.PEENE@GMAIL.com>
2017-09-18 16:25:00 +0200

    Add homepage and navbar, delete admin sign up route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
   devise_for :students, path: 'students', controllers: { sessions: "students/sessions" }
-  devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions" }
+  devise_for :admins, path: 'admins', skip: [:registrations], controllers: { sessions: "admins/sessions" }
 end
