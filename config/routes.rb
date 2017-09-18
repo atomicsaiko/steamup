ea661f4622c57ee095d8c2e03c22758628bc438a
atomicsaiko <atomicsaiko@gmail.com>
2017-09-18 15:13:34 +0200

    Create Devise Admin model and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   devise_for :students, path: 'students', controllers: { sessions: "students/sessions" }
+  devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions" }
 end
