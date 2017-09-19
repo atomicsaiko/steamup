6af6b5f1597cd2b9f8a0637b2b28d7485c554ead
EAPCMC <E.A.PEENE@GMAIL.com>
2017-09-19 14:10:19 +0200

    Working on storing

--- app/models/studentpair.rb
+++ app/models/studentpair.rb
@@ -1,8 +1,14 @@
 class Studentpair < ApplicationRecord
+
   def get_random_student
     Student.order("RANDOM()").limit(1).ids.join.to_i
   end
+
   def create_random_studentpair
 
+  end
+
+  def store_student_pair(student1, student2)
+    Studentpair.new(student1: student1, student2: student2, date:)
   end
 end
