6a1231fbb7310653ff316389847af192df5ef0db
EAPCMC <E.A.PEENE@GMAIL.com>
2017-09-19 11:52:54 +0200

    Working on random_student no array

--- app/models/studentpair.rb
+++ app/models/studentpair.rb
@@ -1,5 +1,9 @@
 class Studentpair < ApplicationRecord
   def get_random_student
-    Student.order("RANDOM()").limit(1).map(&:id)
+    Student.order("RANDOM()").limit(1).map(&id)
+  end
+  def create_random_studentpair
+
+    end
   end
 end
