f33f10aaae03204684e4f483908baf5866f0a6c3
EAPCMC <E.A.PEENE@GMAIL.com>
2017-09-19 12:16:25 +0200

    get_random_student single integer

--- app/models/studentpair.rb
+++ app/models/studentpair.rb
@@ -1,9 +1,8 @@
 class Studentpair < ApplicationRecord
   def get_random_student
-    Student.order("RANDOM()").limit(1).map{ |student| student.send(:id) }
+    Student.order("RANDOM()").limit(1).ids.join.to_i
   end
   def create_random_studentpair
 
-    end
   end
 end
