bfcd407bbfeb3cc6f23f66f58ae916a3766c46cc
atomicsaiko <atomicsaiko@gmail.com>
2017-09-21 15:19:50 +0200

    Create Admin dashboard

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,30 @@
 class AdminsController < ApplicationController
   before_action :authenticate_admin!
+  before_action :get_date # :get_date
+  before_action :get_week_number
 
   def show
     @admin = Admin.find(params[:id])
+    @monday = Studentpair.where(date: '2017-09-25')
+    @tuesday = Studentpair.where(date: '2017-09-26')
+    @wednesday = Studentpair.where(date: '2017-09-27')
+    @thursday = Studentpair.where(date: '2017-09-28')
+    @friday = Studentpair.where(date: '2017-09-29')
+  end
+
+  private
+
+  def get_date
+    # Date.today.strftime("%A")
+    @date = Date.today
+    @dayofweek = Date.today.strftime("%A")
+  end
+
+  def get_date_dummy
+    @date = '2019-09-21'
+  end
+
+  def get_week_number
+    @weeknumber = Time.now.strftime('%U')
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,16 @@
 class StudentsController < ApplicationController
   before_action :authenticate_student!
-  before_action :get_date_dummy # :get_date
+  before_action :get_date # :get_date
 
   def show
     @student = Student.find(params[:id])
-    # @student2 = Studentpair.find_by(date: '2019-01-01')
     @student2 = Studentpair.find_by(date: @date)
     @partner = Studentpair.find_by(date: @date, student1: @student.id.to_i) ||
                Studentpair.find_by(date: @date, student2: @student.id.to_i)
     @partner1_email = Student.find(@partner.student1).email
     @partner2_email = Student.find(@partner.student2).email
-    #@partner = Studentpair.find_by(date: @date, student1: 6)
+
+    # Needs a catch
   end
 
   private
@@ -21,6 +21,6 @@ class StudentsController < ApplicationController
   end
 
   def get_date_dummy
-    @date = '2019-01-01'
+    @date = '2019-09-21'
   end
 end
