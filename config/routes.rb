e1ca13c2096ade9e963c93b336cee1df9cabf971
atomicsaiko <atomicsaiko@gmail.com>
2017-09-22 14:12:08 +0200

    Move the create and delete button for Studentpairs to the Admin show page

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
 
   get '/admins/promote/:id', to: 'admins#promote_to_admin', as: :promote
   get '/admins/demote/:id', to: 'admins#demote_to_student', as: :demote
+
+  get '/admins/:id/studentpairs/create', to: 'admins#create_studentpairs', as: :admins_studentpairs_create
+  get '/admins/:id/studentpairs/delete', to: 'admins#delete_studentpairs', as: :admins_studentpairs_delete
 end
