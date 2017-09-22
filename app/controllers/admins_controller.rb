8b94e7e02c61d903900a31ffc3caba52e664cf87
atomicsaiko <atomicsaiko@gmail.com>
2017-09-22 12:06:58 +0200

    Create the function to promote Student to Admin. And create the index view template for Admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,11 @@
 class AdminsController < ApplicationController
   before_action :authenticate_admin!
 
+  def index
+    @students = Student.all
+    @admins = Admin.all
+  end
+
   def show
     @admin = Admin.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions" }
 
   resources :students, only: [:show]
-  resources :admins, only: [:show]
+  resources :admins, only: [:index, :show]
+
+  get '/students/:id/promote', to: 'admin#promote_to_admin', as: :student_promote
 end
